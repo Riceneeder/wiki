fa977b29288a9282e30f1e4d691f561666ea569a
梵高先生 <845541909@qq.com>
2022-01-25 14:26:19 +0800

    1

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    // 站点配置
+    lang: 'zh-CN',
+    title: '乐下载虚拟主机',
+    description: '这是一个提供免费虚拟主机的公益项目',
+    //base: '/',
+     
+    // 主题和它的配置
+    theme: '@vuepress/theme-default',
+    themeConfig: {
+      logo: 'https://p.qlogo.cn/gh/707537805/950336404/100',
+      displayAllHeaders: true ,// 默认值：false,
+      sidebar: 'auto',
+      lastUpdated: true,
+      navbar: [
+        {
+          text: '指南',
+          link: '/wiki/introduce.md',
+        }
+      ],
+      sidebar: [
+        // SidebarItem
+        {
+          text: '指南',
+          link: null,
+          children: [
+            '/wiki/introduce.md',
+            '/wiki/QA.md',
+            '/wiki/start.md'
+          ],
+        },
+      ],
+    },
+  }
---
+++ docs/README.md
@@ -0,0 +1,24 @@
+---
+home: true
+heroImage: https://p.qlogo.cn/gh/707537805/950336404/100
+heroText: 乐下载虚拟主机
+tagline: 即便世界末日也不会影响到我们的服务质量！
+actions:
+  - text: 快速上手
+    link: /wiki/introduce.html
+    type: primary
+features:
+- title: 100% 在线率
+  details: 我们提供100%在线率的服务器，云服务，云计算等多种有质量保证的产品。
+- title: 安全保护
+  details: 人类文明有史以来最强大的防火墙已经被集装上了您的服务器中！
+- title: 独立客服
+  details: 客服人员随时随地等待您的指示，一个客服不会同时面对超过2个客户！
+- title: 内容分发
+  details: 所有的资源内容都会平均的分发到每一位的用户手中。
+- title: 各种集成
+  details: 支持一键搭建各类型环境以及网站，包括云塔这种优秀的销售系统！
+- title: 快速加载
+  details: 购买的服务器平均延迟不会超过100000毫秒，使用CN2双向线路。
+footer: Copyright © 2022 | 官方QQ群：950336404
+---
